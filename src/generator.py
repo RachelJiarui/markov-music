8d706126ebadfd1dd7265d21a6b171cd0fc12e64
hmcclew <122323895+hmcclew@users.noreply.github.com>
2023-11-24 09:57:48 -0500

    using more than one input file

--- src/generator.py
+++ src/generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# This class handles the generation of a new song given a markov chain
+# This class handles the generation of a new song given a list of markov chains
 # containing the note transitions and their frequencies.
 
 from markov_chain import MarkovChain
@@ -9,13 +9,13 @@ import mido
 
 class Generator:
 
-    def __init__(self, markov_chain):
-        self.markov_chain = markov_chain
+    def __init__(self, markov_chains):
+        self.markov_chains = markov_chains
 
     @staticmethod
-    def load(markov_chain):
-        assert isinstance(markov_chain, MarkovChain)
-        return Generator(markov_chain)
+    def load(markov_chains):
+        assert all(isinstance(chain, MarkovChain) for chain in markov_chains)
+        return Generator(markov_chains)
 
     def _note_to_messages(self, note):
         return [
@@ -31,20 +31,26 @@ class Generator:
             last_note = None
             # Generate a sequence of 100 notes
             for i in range(100):
-                new_note = self.markov_chain.get_next(last_note)
+                # Randomly choose a markov chain for each iteration
+                selected_chain = random.choice(self.markov_chains)
+                new_note = selected_chain.get_next(last_note)
                 track.extend(self._note_to_messages(new_note))
             midi.tracks.append(track)
             midi.save(filename)
 
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 3:
+    if len(sys.argv) >= 3:
         # Example usage:
-        # python generator.py <in.mid> <out.mid>
+        # python generator.py <in1.mid> <in2.mid> ... <out.mid>
         from parser import Parser
-        chain = Parser(sys.argv[1]).get_chain()
-        Generator.load(chain).generate(sys.argv[2])
+
+        # Load multiple markov chains from input MIDI files
+        chains = [Parser(file).get_chain() for file in sys.argv[1:-1]]
+
+        # Generate a new song using the provided markov chains
+        Generator.load(chains).generate(sys.argv[-1])
         print('Generated markov chain')
     else:
         print('Invalid number of arguments:')
-        print('Example usage: python generator.py <in.mid> <out.mid>')
+        print('Example usage: python generator.py <in1.mid> <in2.mid> ... <out.mid>')
